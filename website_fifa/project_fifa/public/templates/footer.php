23fdc6b32ab72eff6a1b38d4ec178aee02c45e43
Sjoerdh95 <sjoerdh.sh@gmail.com>
2017-05-04 11:34:11 +0200

    css update

--- website_fifa/project_fifa/public/templates/footer.php
+++ website_fifa/project_fifa/public/templates/footer.php
@@ -1,12 +1,12 @@
 <footer>
-    <div class="container">
+    <div class="container row-spaced">
         <ul class="row-spaced">
             <li><a href="#">link1</a></li>
             <li><a href="#">link2</a></li>
             <li><a href="#">link3</a></li>
             <li><a href="#">link4</a></li>
         </ul>
-        <p>Dreamteam Design</p>
+        <p>Dreamteam Design &copy;</p>
     </div>
 </footer>
 </div>
