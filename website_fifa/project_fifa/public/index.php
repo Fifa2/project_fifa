3cab8d845f17eade255abba43322fdf73b2d3200
Sjoerdh95 <sjoerdh.sh@gmail.com>
2017-05-04 11:13:04 +0200

    website update

--- website_fifa/project_fifa/public/index.php
+++ website_fifa/project_fifa/public/index.php
@@ -1,8 +1,21 @@
-<?php require(realpath(__DIR__) . '/templates/header.php'); ?>
+<?php require(realpath(__DIR__) . '/templates/header.php');
+require ('../app/database.php')?>
 
 <div class="main-content">
+    <div class="container">
+        <ul>
+            <?php
+            $sql = "SELECT * FROM tbl_teams ";
+            $items = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
+            foreach ($items as $item)
+            {
+                $name = $item['name'];
+                echo '<ul>' . $name . '</ul>';
+            }
+            ?>
+        </ul>
+    </div>
 </div>
-
 
 <?php require(realpath(__DIR__) . '/templates/footer.php');
