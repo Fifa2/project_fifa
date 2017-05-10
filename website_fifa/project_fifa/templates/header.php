a025cf0a4ff57198324b655cf9b62d96a7cf3a1d
Sjoerdh95 <sjoerdh.sh@gmail.com>
2017-05-10 15:33:23 +0200

    finished header

--- website_fifa/project_fifa/templates/header.php
+++ website_fifa/project_fifa/templates/header.php
@@ -2,23 +2,31 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Project Fifa</title>
-    <!-- you can link bootstrap if you want.   -->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
 <!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
-
-    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 <div class="wrapper">
     <div class="header">
-        <div class="container row-spaced">
-            <h1>FIFA</h1>
+        <div class="container">
+            <div class="header-wrapper row-spaced">
+                <h1>FIFA</h1>
                 <ul class="row-spaced">
-                    <li><a href="#">link1</a></li>
-                    <li><a href="#">link2</a></li>
-                    <li><a href="#">link3</a></li>
-                    <li><a href="#">link4</a></li>
+                    <li><a href="#">Link1</a></li>
+                    <li><a href="#">Link2</a></li>
+                    <li><a href="#">Link3</a></li>
+                    <li><a href="#">Link4</a></li>
                 </ul>
+            </div>
         </div>
     </div>
