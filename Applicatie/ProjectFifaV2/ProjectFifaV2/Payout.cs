5b6c2d2f827e444691fd6f2da0865c2e8448bd55
Sjoerdh95 <sjoerdh.sh@gmail.com>
2017-05-16 15:54:02 +0200

    payout.cs toegevoegd

---
+++ Applicatie/ProjectFifaV2/ProjectFifaV2/Payout.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectFifaV2
+{
+    class Payout
+    {
+        protected int betAmount;
+        protected int winningTeam;
+        protected int multiplier;
+        protected int prediction;
+
+        public void PayoutBet(int prediction, int betAmount, int winningTeam)
+        {
+            this.prediction = prediction;
+            this.betAmount = betAmount;
+            this.winningTeam = winningTeam;
+
+            if (prediction == winningTeam)
+            {
+                multiplier = 2;
+            }
+            else
+            {
+                multiplier = 0;
+            }
+
+            int winnings = (betAmount * multiplier);
+            //uitbetalen.
+
+        }
+
+
+    }
+}
