1f0f7357a5a1075380288f2d3834c98afde26292
Sjoerdh95 <sjoerdh.sh@gmail.com>
2017-05-30 20:38:33 +0200

    displayteams startup

--- website_fifa/project_fifa/app/Teams.php
+++ website_fifa/project_fifa/app/Teams.php
@@ -21,7 +21,16 @@ class Teams
         require_once '../app/database.php';
         $sql = "INSERT INTO tbl_teams (poule_id, teamname, points, doesexist) VALUES ('$this->poolID', '$this->teamName', '$this->points', '$this->doesExist')";
         $db->query($sql);
-//        return true;
+    }
+
+    function displayteams()
+    {
+        global $db;
+        require_once '../app/database.php';
+        $sql = "SELECT tbl_players.id, tbl_players.first_name, tbl_players.last_name FROM tbl_users INNER JOIN tbl_teams ON tbl_players.team_id = tbl_teams.id";
+        $db->query($sql);
+
+        echo "";
     }
     //DeleteTeams();
     function DeleteTeams()
