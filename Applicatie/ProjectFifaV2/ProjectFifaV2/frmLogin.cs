28746e1c31e4f87fe017932dfe7bcccb47619a8a
Unknown <cornesierat@gmail.com>
2017-06-07 23:29:04 +0200

    frmlogin update

--- Applicatie/ProjectFifaV2/ProjectFifaV2/frmLogin.cs
+++ Applicatie/ProjectFifaV2/ProjectFifaV2/frmLogin.cs
@@ -23,7 +23,6 @@ namespace ProjectFifaV2
             dbh = new DatabaseHandler();
             frmAdmin = new frmAdmin();
             frmRanking = new frmRanking();
-            //frmPlayer = new frmPlayer(frmRanking);
         }
 
         private void btnRegister_Click(object sender, EventArgs e)
