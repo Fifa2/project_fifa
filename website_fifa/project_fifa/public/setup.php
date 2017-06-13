6561413fa7e0abd316de3ed901a6d9362d113a56
Sjoerdh95 <sjoerdh.sh@gmail.com>
2017-06-13 23:01:28 +0200

    styling

--- website_fifa/project_fifa/public/setup.php
+++ website_fifa/project_fifa/public/setup.php
@@ -2,11 +2,11 @@
 require('../app/database.php') ?>
 
 <div class="setup-items">
-    <div class="container whiteborder pt">
-        <h2>
-            How To Get Started:
-        </h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet corporis eaque est ex fuga, fugit maxime mollitia odio perspiciatis, possimus quia quibusdam quisquam quod? Consequuntur dignissimos reiciendis repellat reprehenderit voluptas.</p>
+    <div class="container whiteborder row-spaced pt">
+            <h2>
+                How To Get Started:
+            </h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet corporis eaque est ex fuga, fugit maxime mollitia odio perspiciatis, possimus quia quibusdam quisquam quod? Consequuntur dignissimos reiciendis repellat reprehenderit voluptas.</p>
     </div>
 </div>
 <div class="setup-items">
@@ -15,7 +15,7 @@ require('../app/database.php') ?>
             Add Teams For Your Tournament:
         </h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo ipsam iure magni minus nemo soluta suscipit ullam vitae voluptatibus. Aliquam harum non nostrum, quibusdam ratione recusandae repellat repellendus tempore vero!</p>
-        <a href="">Go!</a>
+        <a href="../admin/addteams.php">Go!</a>
     </div>
 </div>
 <div class="setup-items">
@@ -24,7 +24,7 @@ require('../app/database.php') ?>
             Add Players For Your Tournament:
         </h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo ipsam iure magni minus nemo soluta suscipit ullam vitae voluptatibus. Aliquam harum non nostrum, quibusdam ratione recusandae repellat repellendus tempore vero!</p>
-        <a href="">Go!</a>
+        <a href="../admin/addplayers.php">Go!</a>
     </div>
 </div>
 <div class="setup-items">
@@ -33,7 +33,7 @@ require('../app/database.php') ?>
             Assign players to teams:
         </h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo ipsam iure magni minus nemo soluta suscipit ullam vitae voluptatibus. Aliquam harum non nostrum, quibusdam ratione recusandae repellat repellendus tempore vero!</p>
-        <a href="">Go!</a>
+        <a href="../admin/assignteams.php">Go!</a>
     </div>
 </div>
 <div class="setup-items">
@@ -42,7 +42,7 @@ require('../app/database.php') ?>
             Generate Your Match Schedule:
         </h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo ipsam iure magni minus nemo soluta suscipit ullam vitae voluptatibus. Aliquam harum non nostrum, quibusdam ratione recusandae repellat repellendus tempore vero!</p>
-        <a href="">Go!</a>
+        <a href="../app/matchgenerator.php">Go!</a>
     </div>
 </div>
 <?php require(realpath(__DIR__) . '/../templates/footer.php');
