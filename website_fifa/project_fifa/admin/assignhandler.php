bded041c604ee13524444e1227e4a8889a214035
d226340 <d226340@edu.rocwb.nl>
2017-06-14 15:47:33 +0200

    assignhandler.php added

--- website_fifa/project_fifa/admin/assignhandler.php
+++ website_fifa/project_fifa/admin/assignhandler.php
@@ -1,3 +1,19 @@
 <?php
-var_dump($_POST);
-//if(isset($_POST['']) && !empty($_POST['teamname']))
+
+if ((isset($_POST['Team']) && !empty($_POST['Team'])) && (isset($_POST['1']) && !empty($_POST['1'])) && (isset($_POST['2']) && !empty($_POST['2'])) && (isset($_POST['3']) && !empty($_POST['3'])) && (isset($_POST['4']) && !empty($_POST['4'])) && (isset($_POST['5']) && !empty($_POST['5'])))
+{
+    require ('../app/database.php');
+    global $db;
+
+    $teamid = $_POST['Team'];
+    $players = array($_POST['1'],$_POST['2'],$_POST['3'],$_POST['4'],$_POST['5']);
+
+    require_once '../app/database.php';
+
+    var_dump($teamid);
+
+    foreach ($players as $player) {
+        $sql = "UPDATE tbl_players SET team_id = $teamid where id = $player";
+        $db->query($sql);
+    }
+}
