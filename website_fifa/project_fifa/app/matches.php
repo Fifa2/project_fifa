b6cb415b20703577272caae9c8eec2f0589313da
Sjoerdh95 <sjoerdh.sh@gmail.com>
2017-06-14 16:55:13 +0200

    schedule

--- website_fifa/project_fifa/app/matches.php
+++ website_fifa/project_fifa/app/matches.php
@@ -30,8 +30,18 @@ class matches
         $db->query($sql);
     }
 
-    function EditMatch()
+    function DisplaySchedule()
     {
-//     also add code here please
+        global $db;
+        require_once '../app/database.php';
+        $sql = "SELECT tbl_matches.team_id_a, teams.teamname, tbl_teams.teamname, tbl_matches.team_id_b FROM tbl_matches INNER JOIN tbl_teams as teams ON tbl_matches.team_id_a = teams.id INNER JOIN tbl_teams ON tbl_matches.team_id_b = tbl_teams.id";
+        $items = $db->query($sql)->fetchAll();
+        foreach ($items as $item)
+        {
+            var_dump($item);
+            $teamaid = $item[0];
+            var_dump($teamaid);
+        }
+
     }
 }
---
+++ website_fifa/project_fifa/public/schedule.php
@@ -0,0 +1,17 @@
+<?php require(realpath(__DIR__) . '/../templates/header.php');
+require('../app/utility.php');
+require('../app/matches.php');
+$utility = new \project_fifa\utility();
+$utility->loginCheck();
+$matches = new \project_fifa\matches();
+
+?>
+<div class="schedule">
+    <div class="container pt">
+        <?php $matches->DisplaySchedule();?>
+    </div>
+</div>
+<?php require(realpath(__DIR__) . '/../templates/footer.php');
+?>
+
+
